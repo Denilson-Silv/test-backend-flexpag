7f09e6332df024e834445db18b29a03f5d9aa1d8
Denilson-Silv <107802815+Denilson-Silv@users.noreply.github.com>
2022-07-27 16:34:58 -0300

    Add files via upload

---
+++ flexpag-desafio-backend-main/src/main/java/com/flexpag/paymentscheduler/controller/AgendmentoController.java
@@ -0,0 +1,67 @@
+package com.flexpag.paymentscheduler.controller;
+
+import java.net.URI;
+
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.flexpag.paymentscheduler.domain.Agendamento;
+import com.flexpag.paymentscheduler.domain.dtos.AgendamentoDTO;
+
+import com.flexpag.paymentscheduler.service.agendamentoServic;
+
+@RestController
+@RequestMapping(value = "/agendamento")
+public class AgendmentoController {
+	@Autowired
+	private agendamentoServic service;
+
+	@GetMapping
+	public ResponseEntity<List<AgendamentoDTO>> findAll() {
+		List<Agendamento> list = service.findAll();
+		List<AgendamentoDTO> listDTO = list.stream().map(obj -> new AgendamentoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AgendamentoDTO> finById(@PathVariable Long id) {
+		Agendamento obj = service.findById(id);
+		return ResponseEntity.ok().body(new AgendamentoDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<AgendamentoDTO> post(@Validated @RequestBody AgendamentoDTO objDTO) {
+		Agendamento newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	 @PutMapping(value = "/{id}")
+	 public ResponseEntity<AgendamentoDTO> update(@PathVariable(value = "id") long
+	 id, @Validated @RequestBody AgendamentoDTO objDTO) {
+	 Agendamento obj = service.update(id, objDTO);
+	 return ResponseEntity.ok().body(new AgendamentoDTO(obj));
+	 }
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Object> delete(@PathVariable(value = "id") long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+}
---
+++ flexpag-desafio-backend-main/src/main/java/com/flexpag/paymentscheduler/domain/enuns/Status.java
@@ -0,0 +1,6 @@
+package com.flexpag.paymentscheduler.domain.enuns;
+
+public enum Status {
+	PENDING , PAID;
+
+}
